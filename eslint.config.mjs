b8cd348535e7b1a069f553f536eda6715d58abe5
Pazazamadaz <52623509+Pazazamadaz@users.noreply.github.com>
2025-04-23 16:21:04 +0100

    configuration

---
+++ eslint.config.mjs
@@ -0,0 +1,25 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import { defineConfig } from 'eslint/config';
+
+export default defineConfig([
+  {
+    files: ['**/*.{js,mjs,cjs,ts}'],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+      globals: globals.node,
+    },
+    plugins: {
+      '@typescript-eslint': tseslint.plugin,
+    },
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+    },
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+]);
